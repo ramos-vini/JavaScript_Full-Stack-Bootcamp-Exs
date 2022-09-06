2ee11b86117e325766ef170ce12f9c0bb59afd20
Vinicius Ramos <ramoscontrole@gmail.com>
2022-09-06 17:04:25 -0300

    ex1.1 script file

---
+++ mod01/ex1.1/script.js
@@ -0,0 +1,13 @@
+ // descricao do todo
+
+    // Adicione uma variável de tipo string para salvar seu nome
+    let nome = "Vinícius";
+
+    // Adicione uma variável de tipo inteiro para salvar sua idade
+    let idade = parseInt("21");
+
+    // Adicione uma variável de tipo date para armazenar a data de seu nascimento
+    let data = new Date("06-23-2001");
+
+    // Utilize a função console para exibir uma mensagem informando seu nome, idade e data de nascimento
+    console.log(`Meu nome é ${nome}, tenho ${idade} anos e nasci em ${data.getDate()}/0${data.getMonth() + 1}/${data.getFullYear()}.`);
